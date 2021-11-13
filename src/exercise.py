c52257110ff96e6bdf4abd77d46ae49bc0395e17
marioservini <31860@bridgend.ac.uk>
2021-11-13 16:22:34 +0000

    complete

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,9 @@
 def main():
-    #write your code below this line
+    # write your code below this line
+    num = int(input('Enter a no:\n>'))
+    for i in range(num, 101):
+        print(i)
+
 
 if __name__ == '__main__':
     main()
